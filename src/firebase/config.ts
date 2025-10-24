23a39fcf1ba80f87eb4cb9bbf9437d71f7966958
G Mahidhar Reddy B.Tech CS (AIML) <mahidhar.btech2024@spsu.ac.in>
2025-10-24 14:12:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8584639697-f6ebb",
+  "appId": "1:746227944459:web:c6c8d0b1875deea0ca0b0e",
+  "apiKey": "AIzaSyD57EEcH8RsuYHLNTKCXv1Kzh0G4h1o8hU",
+  "authDomain": "studio-8584639697-f6ebb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "746227944459"
+};
